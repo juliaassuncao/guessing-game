4f0c5e230f131e8efa9adf8659b6b27c3c29b67f
Julia Assunção <juliaassuncao256@gmail.com>
2025-05-07 18:57:51 -0300

    feat: estiliza o logo e importa estilização no index do componente Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,10 @@
 import logo from "../../assets/logo.png"
 
+import styles from "./styles.module.css"
+
 export function Header() {
   return(
-    <div>
+    <div className={styles.container}>
       <img src={logo} alt="Logo" />
     </div>
   )
