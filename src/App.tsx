48ff86bff1bc918b33d1877a3dec3ef9dfb52c62
Julia Assunção <juliaassuncao256@gmail.com>
2025-05-08 15:04:13 -0300

    feat: cria componente Tip e cria pasta de componente Letter

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
 import styles from "./app.module.css"
 
 import { Header } from "./components/Header"
+import { Tip } from "./components/Tip"
 
 export function App() {
+
   function handleRestartgame() {
     alert("Reiniciar o jogo!")
   }
-
 
   return(
     <div className={styles.container}>
       <main>
         <Header current={5} max={10} onRestart={handleRestartgame} />
+        <Tip tip="Biblioteca para criar interfaces Web com Javascript." />
       </main>
     </div>
   )
