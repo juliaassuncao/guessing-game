81eb0b4965c026a6645290f2df6e1ca69af3d944
Julia Assunção <juliaassuncao256@gmail.com>
2025-05-08 15:25:50 -0300

    feat: cria lógica de componente Tip, estiliza e adiciona no App()

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import styles from "./app.module.css"
 
 import { Header } from "./components/Header"
 import { Tip } from "./components/Tip"
+import { Letter } from "./components/Letter"
 
 export function App() {
 
@@ -13,7 +14,14 @@ export function App() {
     <div className={styles.container}>
       <main>
         <Header current={5} max={10} onRestart={handleRestartgame} />
+        
         <Tip tip="Biblioteca para criar interfaces Web com Javascript." />
+        
+        <Letter value="R" />
+        <Letter value="E" />
+        <Letter value="A" />
+        <Letter value="C" />
+        <Letter value="T" />
       </main>
     </div>
   )
